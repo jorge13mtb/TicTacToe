cbefefab34bce236372ed30b7536ce0bb175cc5f
Jorge Chavarria <jorge13mtb@hotmail.com>
2013-07-25 14:24:07 -0600

    refactoring algorithm to find the winner of the game

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -15,6 +15,8 @@ class Tictactoe
     @board = [["-","-","-"],["-","-","-"],["-","-","-"]]
 	  @game_winner = :no_player
     @turns = 0
+    @last_row = -1
+    @last_column = -1
   end
 
 
@@ -22,13 +24,23 @@ class Tictactoe
 	  if @board[row_position][column_position] == EMPTY_BOX
 	    @board[row_position][column_position] = game_cell
       @turns += 1
+      @last_row = row_position
+      @last_column = column_position
     end
   end
 
 
   def is_there_a_winner?
     if @turns >= MINIMUM_NUMBER_OF_TURNS_NEEDED_TO_WIN
-      winner_in_row? or winner_in_column? or winner_in_diagonal_1? or winner_in_diagonal_2?
+
+      row_array = [@board[@last_row][0], @board[@last_row][1], @board[@last_row][2]]
+      column_array = [@board[0][@last_column], @board[1][@last_column], @board[2][@last_column]]
+
+      diagonal_1_array = [@board[0][0], @board[1][1], @board[2][2]] if played_in_diagonal_1?
+      diagonal_2_array = [@board[0][2], @board[1][1], @board[2][0]] if played_in_diagonal_2?
+
+      winner_in_array? row_array or winner_in_array? column_array or 
+      winner_in_array? diagonal_1_array or winner_in_array? diagonal_2_array
     end
   end
 
@@ -44,9 +56,9 @@ class Tictactoe
 
 
   def clean_the_board
-	  for actual_row in (0..2)
-	    for actual_column in (0..2)
-		    @board[actual_row][actual_column] = EMPTY_BOX
+	  for current_row in (0..2)
+	    for current_column in (0..2)
+		    @board[current_row][current_column] = EMPTY_BOX
 	    end
   	end
     @turns = 0
@@ -56,46 +68,22 @@ class Tictactoe
 
   private
 
-  def winner_in_row?
-    for actual_row in (0..2)
-	    if all_elements_are_equals?(@board[actual_row]) and not empty_box?(@board[actual_row])
-	      @game_winner = @board[actual_row][0]
-	      return true
-      end
-	   end
-  false
+  def played_in_diagonal_1?
+    @last_row - @last_column = 0
   end
 
 
-  def winner_in_column?
-    for actual_column in (0..2)
-      actual_column_values = [@board[0][actual_column], @board[1][actual_column], @board[2][actual_column]]
-	    if all_elements_are_equals?(actual_column_values) and not empty_box?(actual_column_values)
-	      @game_winner = @board[0][actual_column]
-		    return true 
-	    end
-	  end
-	false
+  def played_in_diagonal_2?
+    (@last_row = 1 and @last_column = 1) or @last_row - @last_column = -2 or @last_row - @last_column = 2
   end
 
 
-  def winner_in_diagonal_1?
-    diagonal_1_values = [@board[0][0], @board[1][1], @board[2][2]]
-    if all_elements_are_equals?(diagonal_1_values) and not empty_box?(diagonal_1_values)
-	    @game_winner = @board[0][0]
-	    return true
-	  end
-  false
-  end
-
-
-  def winner_in_diagonal_2?
-    diagonal_2_values = [@board[0][2], @board[1][1], @board[2][0]]
-	  if all_elements_are_equals?(diagonal_2_values) and not empty_box?(diagonal_2_values)
-	    @game_winner = @board[0][2]
+  def winner_in_array? array_values
+	  if all_elements_are_equals?(array_values) and not empty_box?(array_values)
+	    @game_winner = array_values[0][0]
     	return true
-     end
-  false
+    end
+    false
   end
 
 
