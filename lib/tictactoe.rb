6d00abc46f4d5c1c8d07229627edbdbd1353576b
Jorge Chavarria <jorge13mtb@hotmail.com>
2013-07-25 09:59:15 -0600

    Changing method's names

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -4,118 +4,114 @@
 =end
 
 EMPTY_BOX = "-"
-MAX_NUMBER_OF_PIECES_IN_TICTACTOE = 9
+MAX_NUMBER_OF_cellS_IN_TICTACTOE = 9
 
 class Tictactoe
 
-    attr_accessor :board
+  attr_accessor :board
 
-    def initialize
-        @board = [["-","-","-"],["-","-","-"],["-","-","-"]]
-	@game_winner = :no_player
+  def initialize
+    @board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+	  @game_winner = :no_player
+  end
+
+
+  def put_cell_at_game(row_position, column_position, game_cell)
+	  if @board[row_position][column_position] == EMPTY_BOX
+	    @board[row_position][column_position] = game_cell
     end
+  end
 
 
-    def put_piece_at_game(row_position, column_position, game_piece)
-	if @board[row_position][column_position] == EMPTY_BOX
-	    @board[row_position][column_position] = game_piece
-        end
-    end
+  def is_there_a_winner?
+    winner_in_row? or winner_in_column? or winner_in_diagonal_1? or winner_in_diagonal_2?
+  end
 
 
-    def is_there_a_winner?
-        is_there_a_winner_in_row? or is_there_a_winner_in_column? or is_there_a_winner_in_diagonal_1? or is_there_a_winner_in_diagonal_2? 
-    end
+  def winner
+    @game_winner
+   end
 
 
-    def who_is_the_winner?
-        @game_winner
-    end
+  def tie?
+	  not is_there_a_winner? and the_board_is_full?
+  end
 
 
-    def is_there_a_tie?
-	not is_there_a_winner? and the_board_is_full?
-    end
- 
-
-    def clean_the_board
-	for actual_row in (0..2)
+  def clean_the_board
+	  for actual_row in (0..2)
 	    for actual_column in (0..2)
-		@board[actual_row][actual_column] = EMPTY_BOX
+		    @board[actual_row][actual_column] = EMPTY_BOX
 	    end
-	end
-    end    
+  	end
+  end
 
-#                    *****                     These are private functions                    *****
+#          *****          These are private functions          *****
 
-    private
+  private
 
-    def is_there_a_winner_in_row?
-        for actual_row in (0..2)
-	    if all_elements_are_equals?(@board[actual_row]) and not is_there_an_empty_box?(@board[actual_row])
-	        @game_winner = @board[actual_row][0]
-	        return true
-            end
-	end
-	false
-    end
+  def winner_in_row?
+    for actual_row in (0..2)
+	    if all_elements_are_equals?(@board[actual_row]) and not empty_box?(@board[actual_row])
+	      @game_winner = @board[actual_row][0]
+	      return true
+      end
+	   end
+  false
+  end
 
 
-    def is_there_a_winner_in_column?
-        for actual_column in (0..2)
-            actual_column_values = [@board[0][actual_column], @board[1][actual_column], @board[2][actual_column]]
-  
-	    if all_elements_are_equals?(actual_column_values) and not is_there_an_empty_box?(actual_column_values)
-	        @game_winner = @board[0][actual_column]
-		return true 
+  def winner_in_column?
+    for actual_column in (0..2)
+      actual_column_values = [@board[0][actual_column], @board[1][actual_column], @board[2][actual_column]]
+	    if all_elements_are_equals?(actual_column_values) and not empty_box?(actual_column_values)
+	      @game_winner = @board[0][actual_column]
+		    return true 
 	    end
-	end
+	  end
 	false
-    end
-    
+  end
 
-    def is_there_a_winner_in_diagonal_1?
-        diagonal_1_values = [@board[0][0], @board[1][1], @board[2][2]]
 
-	if all_elements_are_equals?(diagonal_1_values) and not is_there_an_empty_box?(diagonal_1_values)
+  def winner_in_diagonal_1?
+    diagonal_1_values = [@board[0][0], @board[1][1], @board[2][2]]
+    if all_elements_are_equals?(diagonal_1_values) and not empty_box?(diagonal_1_values)
 	    @game_winner = @board[0][0]
 	    return true
-	end
-	false 
-    end
+	  end
+  false
+  end
 
 
-    def is_there_a_winner_in_diagonal_2?
-        diagonal_2_values = [@board[0][2], @board[1][1], @board[2][0]]
-
-	if all_elements_are_equals?(diagonal_2_values) and not is_there_an_empty_box?(diagonal_2_values)
+  def winner_in_diagonal_2?
+    diagonal_2_values = [@board[0][2], @board[1][1], @board[2][0]]
+	  if all_elements_are_equals?(diagonal_2_values) and not empty_box?(diagonal_2_values)
 	    @game_winner = @board[0][2]
-    	    return true
-        end
-	false
-    end
+    	return true
+     end
+  false
+  end
 
 
-    def all_elements_are_equals?(array)
-	array.uniq.length == 1
-    end
-
- 
-    def is_there_an_empty_box?(array)
-	array.include?(EMPTY_BOX)
-    end
+  def all_elements_are_equals?(array)
+	  array.uniq.length == 1
+  end
 
 
-    def the_board_is_full?
-        pieces_in_game = 0
-	for current_row in (0..2)
+  def empty_box?(array)
+	  array.include?(EMPTY_BOX)
+  end
+
+
+  def the_board_is_full?
+    cells_in_game = 0
+	  for current_row in (0..2)
 	    for current_column in (0..2)
-		if @board[current_row][current_column] != EMPTY_BOX
-		    pieces_in_game += 1
-		end
+	  	  if @board[current_row][current_column] != EMPTY_BOX
+		      cells_in_game += 1
+		    end
 	    end
-	end
-	pieces_in_game == MAX_NUMBER_OF_PIECES_IN_TICTACTOE
-    end
+	  end
+  cells_in_game == MAX_NUMBER_OF_cellS_IN_TICTACTOE
+  end
 end
-
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -9,37 +9,38 @@ require 'tictactoe.rb'
 
 describe Tictactoe do
 
-    before do
-	@tictactoe = Tictactoe.new
-    end
+  before do
+	  @tictactoe = Tictactoe.new
+   end
 
-    describe "initialize the tictactie" do
- 	it "values of the beginning of the tictactoe game" do
+  describe "initialize the tictactie" do
+ 	  it "values of the beginning of the tictactoe game" do
 	    @tictactoe.should_not be_nil
-	    expect(@tictactoe.board).to eq([["-","-","-"],["-","-","-"],["-","-","-"]])
-	end
+	    #expect(@tictactoe.board).to eq([["-","-","-"],["-","-","-"],["-","-","-"]])
+	    @tictactoe.board.should == [["-","-","-"],["-","-","-"],["-","-","-"]]
     end
+  end
 
     describe "to play" do
         before { @tictactoe.board = [["X","0","-"],["-","-","-"],["X","0","X"]] }
 
-	it "put two pieces in different places" do
-	    @tictactoe.put_piece_at_game(0,2,"X")
-	    @tictactoe.put_piece_at_game(1,0,"0")
+	it "put two cells in different places" do
+	    @tictactoe.put_cell_at_game(0,2,"X")
+	    @tictactoe.put_cell_at_game(1,0,"0")
 	    expect(@tictactoe.board).to eq([["X","0","X"],["0","-","-"],["X","0","X"]])
 	end
 
-	it "put two pieces in the same place" do
-	    @tictactoe.put_piece_at_game(1,1,"X")
-	    @tictactoe.put_piece_at_game(1,1,"0")
+	it "put two cells in the same place" do
+	    @tictactoe.put_cell_at_game(1,1,"X")
+	    @tictactoe.put_cell_at_game(1,1,"0")
 	    expect(@tictactoe.board).to eq([["X","0","-"],["-","X","-"],["X","0","X"]])
 	end  
 
 	it "put a complete game" do
-	    @tictactoe.put_piece_at_game(0,2,"X")
-	    @tictactoe.put_piece_at_game(1,0,"0")
-	    @tictactoe.put_piece_at_game(1,1,"X")
-	    @tictactoe.put_piece_at_game(1,2,"0")
+	    @tictactoe.put_cell_at_game(0,2,"X")
+	    @tictactoe.put_cell_at_game(1,0,"0")
+	    @tictactoe.put_cell_at_game(1,1,"X")
+	    @tictactoe.put_cell_at_game(1,2,"0")
 	    expect(@tictactoe.board).to eq([["X","0","X"],["0","X","0"],["X","0","X"]])
         end
     end
@@ -50,36 +51,36 @@ describe Tictactoe do
 	
 	it "is there a winner in a row?" do
 	    @tictactoe.is_there_a_winner?.should be_false
-	    @tictactoe.put_piece_at_game(2,0,"0")
-	    @tictactoe.put_piece_at_game(2,1,"0")
-	    @tictactoe.put_piece_at_game(2,2,"0")
+	    @tictactoe.put_cell_at_game(2,0,"0")
+	    @tictactoe.put_cell_at_game(2,1,"0")
+	    @tictactoe.put_cell_at_game(2,2,"0")
 	    @tictactoe.is_there_a_winner?.should be_true
-	    expect(@tictactoe.who_is_the_winner?).to eq("0")
+	    expect(@tictactoe.winner).to eq("0")
 	end
 
 	it "is there a winner in a column?" do
 	    @tictactoe.is_there_a_winner?.should be_false
-	    @tictactoe.put_piece_at_game(1,0,"X")
-	    @tictactoe.put_piece_at_game(2,0,"X")
+	    @tictactoe.put_cell_at_game(1,0,"X")
+	    @tictactoe.put_cell_at_game(2,0,"X")
 	    @tictactoe.is_there_a_winner?.should be_true
-	    expect(@tictactoe.who_is_the_winner?).to eq("X")
+	    expect(@tictactoe.winner).to eq("X")
 	end
 
 	it "is there a winner in diagonal 1?" do
 	    @tictactoe.is_there_a_winner?.should be_false
-	    @tictactoe.put_piece_at_game(1,1,"X")
-	    @tictactoe.put_piece_at_game(2,2,"X")
+	    @tictactoe.put_cell_at_game(1,1,"X")
+	    @tictactoe.put_cell_at_game(2,2,"X")
 	    @tictactoe.is_there_a_winner?.should be_true
-	    expect(@tictactoe.who_is_the_winner?).to eq("X")
+	    expect(@tictactoe.winner).to eq("X")
 	end
 
 	it "is there a winner in diagonal 2?" do
 	    @tictactoe.is_there_a_winner?.should be_false
-	    @tictactoe.put_piece_at_game(0,2,"0")
-	    @tictactoe.put_piece_at_game(1,1,"0")
-	    @tictactoe.put_piece_at_game(2,0,"0")
+	    @tictactoe.put_cell_at_game(0,2,"0")
+	    @tictactoe.put_cell_at_game(1,1,"0")
+	    @tictactoe.put_cell_at_game(2,0,"0")
 	    @tictactoe.is_there_a_winner?.should be_true
-	    expect(@tictactoe.who_is_the_winner?).to eq("0")
+	    expect(@tictactoe.winner).to eq("0")
 	end
     end
 
@@ -89,8 +90,8 @@ describe Tictactoe do
 	
 	it "the winner is X" do
 	    @tictactoe.is_there_a_winner?.should be_true
-	    @tictactoe.is_there_a_tie?.should be_false
-	    expect(@tictactoe.who_is_the_winner?).to eq("X")
+	    @tictactoe.tie?.should be_false
+	    expect(@tictactoe.winner).to eq("X")
 	end
     end
 
@@ -99,8 +100,8 @@ describe Tictactoe do
 
 	it "there is a tie" do
 	    @tictactoe.is_there_a_winner?.should be_false
-	    @tictactoe.is_there_a_tie?.should be_true
-	    expect(@tictactoe.who_is_the_winner?).to eq(:no_player)
+	    @tictactoe.tie?.should be_true
+	    expect(@tictactoe.winner).to eq(:no_player)
 	end
 
         it "clean the board" do
